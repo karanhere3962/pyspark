dcc3e32f7adaf1d9e50e88ce5fa51978815b18d9
KaranChettri <karanhere3962@gmail.com>
2022-11-01 08:30:35 +0530

    Adding exception handlers

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import JSONResponse, Response
 from utils.helpers import get_header_details, load_csv_to_spark_session
 from schemas import SQLAPISchema
@@ -6,6 +6,7 @@ from kink import di
 from pyspark.sql.window import Window
 from pyspark.sql.functions import row_number, lit
 from pyspark.sql import utils as sputils
+import traceback
 
 router = APIRouter()
 
@@ -14,34 +15,27 @@ router = APIRouter()
 def run_sql_query(query_details: SQLAPISchema):
     spark_session = di["SPARK_SESSION"]
     with load_csv_to_spark_session(sql_query=query_details.sql_query) as loaded_views:
-
-        # try:
-        df = spark_session.sql(query_details.sql_query)
-        df = df.withColumn("index", row_number().over(Window().orderBy(lit("A"))))
-        # result = []
-        # df = df.rdd.zipWithIndex().toDF()
-        # for row in df.rdd.toLocalIterator():
-        #     if row._2 < query_details.offset:
-        #         continue
-        #     if row._2 >= query_details.limit:
-        #         break
-        #     result.append(row._1.asDict())
-        result = (
-            df.filter(
-                (df.index - 1 >= query_details.offset)
-                & (df.index - 1 < query_details.limit)
+        try:
+            df = spark_session.sql(query_details.sql_query)
+            df = df.withColumn("index", row_number().over(Window().orderBy(lit("A"))))
+            result = (
+                df.filter(
+                    (df.index - 1 >= query_details.offset)
+                    & (df.index - 1 < query_details.limit)
+                )
+                .drop("index")
+                .rdd.map(lambda row: row.asDict())
+                .collect()
             )
-            .drop("index")
-            # .select([column for column in df.columns if column not in {"index"}])
-            .rdd.map(lambda row: row.asDict())
-            .collect()
-        )
-        # except sputils.AnalysisException as e:
-        #     raise e
-        #     return Response(str(e), status_code=400)
-        # except Exception as e:
-        #     print(e)
-        #     return Response(str(e), status_code=500)
+        except sputils.AnalysisException as e:
+            traceback.print_exc()
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+        except Exception as e:
+            traceback.print_exc()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+            )
         return JSONResponse(content=result)
 
 
